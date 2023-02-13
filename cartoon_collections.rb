6f0592d410703bd01dc67044ea6740d9d3a5afea
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-13 12:48:32 +0300

    completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(dwarves)
   # Your code here
+    dwarves.each_with_index do |name, index|
+      puts "#{index + 1}. #{name}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map{ |planeteer_calls| planeteer_calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls list
+  list.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese food_items
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_items.find { |item| cheese_types.include?(item) }
 end
+
+puts roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+puts summon_captain_planet(planeteer_calls)
+puts long_planeteer_calls(["puff", "go", "two", "test", "anothertest", "industrious", "bop"])
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+puts find_the_cheese(soup)
+
+# binding.pry
